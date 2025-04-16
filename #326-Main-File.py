35408dcbfe27435480c27c933f02cca1e73e24b2
carmelloj <carmellojones.22@gmail.com>
2025-04-16 13:42:04 -0400

    Quick docstring on the goal of our Project

--- #326-Main-File.py
+++ #326-Main-File.py
@@ -1,1 +1,8 @@
 #326-Main-File
+
+"""
+    The goal of this project is to provide users with a workout plan tailored to their fitness goals. 
+    We will take information like their preferred location (home/gym), workout frequency (1-5 days a week) and focus area(s). 
+
+
+"""
